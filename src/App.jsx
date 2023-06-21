3c626426d43897f73890f1b8899f37d1da3c194b
nitesh92 <niteshjaiswal9292@gmmail.com>
2023-06-21 15:00:19 +0530

    Keeper app compeleted

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNote) => {
+      return [...prevNote, note];
+    });
+  }
+
+  console.log(notes);
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer()
+{
+    const date = new Date();
+    const currentYear = date.getFullYear();
+   return (<div>
+    <footer>
+    <p>Copyright ⓒ {currentYear}</p>
+    </footer>
+   </div>
+   )
+}
+
+export default Footer;
